902a9d92795e467747e1a410e27895035a2d4ab5
Umair Hameed <71635315+umair406@users.noreply.github.com>
2021-01-02 15:50:56 +0500

    changing

--- jqu.js
+++ jqu.js
@@ -55,3 +55,32 @@ function responseMessage(msg) {
   $('.success-box').fadeIn(200);  
   $('.success-box div.text-message').html("<span>" + msg + "</span>");
 }
+
+$(document).ready(function () {
+
+  $("#sidebar").mCustomScrollbar({
+       theme: "minimal"
+  });
+
+  $('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+  });
+
+});
+$(document).ready(function () {
+
+  $("#sidebar").mCustomScrollbar({
+       theme: "minimal"
+  });
+
+  $('#sidebarCollapse').on('click', function () {
+      // open or close navbar
+      $('#sidebar').toggleClass('active');
+      // close dropdowns
+      $('.collapse.in').toggleClass('in');
+      // and also adjust aria-expanded attributes we use for the open/closed arrows
+      // in our CSS
+      $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+  });
+
+});
